d44bbebb658ad8a1d1294b0a08707fe3000b4797
Benjamin Skipper <BSkipper@commandalkon.com>
2015-10-06 16:05:17 -0500

    categories put and delete now requires login

--- app/routes/categories.server.routes.js
+++ app/routes/categories.server.routes.js
@@ -2,15 +2,16 @@
 
 module.exports = function (app) {
     var categories = require('../../app/controllers/categories.server.controller');
+    var users = require('../../app/controllers/users.server.controller');
 
     app.route('/categories')
         .get(categories.list)
-        .post(categories.create);
+        .post(users.requiresLogin, categories.create);
 
     app.route('/categories/:categoryID')
         .get(categories.read)
-        .put(categories.update)
-        .delete(categories.delete);
+        .put(users.requiresLogin, categories.update)
+        .delete(users.requiresLogin, categories.delete);
 
     //Finish by binding the article middleware
     //What's this? Where the categoryID is present in the URL
