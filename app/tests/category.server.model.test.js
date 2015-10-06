687ef8ad1f35b6c2796a84e40fff31abadf127f6
Benjamin Skipper <BSkipper@commandalkon.com>
2015-10-06 10:24:51 -0500

    finished category.server.model.test

--- app/tests/category.server.model.test.js
+++ app/tests/category.server.model.test.js
@@ -4,19 +4,75 @@
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	Category = mongoose.model('Category');
+    mongoose = require('mongoose'),
+    Category = mongoose.model('Category');
 
 /**
  * Unit tests
  */
-describe('Category Model', function(){
+describe('Category Model', function () {
 
-    it('saves new record');
+    decribe('Saving', function () {
 
-    it('throws validation error when name is empty');
+        it('saves new record', function (done) {
+            var category = new Category({
+                name: 'Beverages',
+                description: 'Soft drinks, coffees, teas, beers, and ales'
+            });
 
-    it('throws validation error when name longer than 15 chars');
+            category.save(function (err, saved) {
+                should.not.exist(err);
+                done();
+            });
+        });
 
-    it('throws validation error for duplicate category name');
+        it('throws validation error when name is empty', function (done) {
+            var category = new Category({
+                description: 'Soft drinks, coffees, teas, beers, and ales'
+            });
+
+            category.save(function (err, saved) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('throws validation error when name longer than 15 chars', function (done) {
+            var category = new Category({
+                name: 'Grains/Cereals/Chocolates'
+            });
+            category.save(function (err, saved) {
+                should.exist(err);
+                err.errors.name.message.should.equal('name must be 15 chars in length or less');
+                done();
+            });
+        });
+
+        it('throws validation error for duplicate category name', function (done) {
+            var category = new Category({
+                name: 'Beverages'
+            });
+
+            category.save(function (err, saved) {
+                should.not.exist(err);
+                done();
+            });
+
+            var duplicate = new Category({
+                name: 'Beverages'
+            });
+
+            duplicate.save(function (err) {
+                err.err.indexOf('$name').should.not.equal(-1);
+                err.err.indexOf('duplicate key error').should.not.equal(-1);
+                should.exist(err);
+                done();
+            });
+        });
+    });
+    afterEach(function(done){
+        // NB this deletes ALL categories (but is run against a test database
+        Category.remove().exec();
+        done();
+    })
 });
